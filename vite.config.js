eb23d2e5beccb7b2ca8df7ad0e107fe807b013ec
Costa Ivo <ivo.costa@creativecapsule.com>
2025-02-02 21:14:49 +0530

    rpa pending code

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        rpa: 'rpa.html',
+        completed_solutions:'completed_solutions.html',
+        code_snippets:'code_snippets.html',
+        pre_requisites:'pre_requisites.html',
+      }
+    }
+  },
   base: '/rpa-workshop/', // Replace 'your-repo-name' with your actual repository name
 });
